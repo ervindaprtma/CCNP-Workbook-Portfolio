240fd6605e7bf44f8e7d2e371ec4efd267b2494f
Ervinda Pratama <93853174+ervindaprtma@users.noreply.github.com>
2025-10-31 09:12:37 +0700

    Changes

--- auto_add.ps1
+++
@@ -1,6 +0,0 @@
-cd "G:\My Drive\CCNP Workbook Project\CCNP-Workbook-Portfolio"
-Get-ChildItem -Recurse -Directory | ForEach-Object {
-    if (-not (Get-ChildItem $_.FullName)) {
-        New-Item -Path "$($_.FullName)\.gitkeep" -ItemType File -Force
-    }
-}
